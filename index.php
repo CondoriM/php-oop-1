b31f5a4938ad408df698864a19aff157fd72c343
CondoriM <condoria.miguel@gmail.com>
2022-05-25 16:10:17 +0200

    completed exercise

--- index.php
+++ index.php
@@ -4,11 +4,18 @@ class Movie {
     public $name;
     public $age;
 
-    function __construct($anno){
+    function __construct(String $name,String $anno){
         $this->age = $anno;
+        $this->name = $name;
     }
-}
 
-$batman = new Movie(2002);
+    public function getName(){
+        return $this->name.' '.$this->age;
+    }
+};
+
+$batman = new Movie('batman','2002');
+$result = $batman->getName();
+echo $result;
 
 ?>
